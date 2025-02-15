5401c64be45971a072e4df2111e419749dbd116c
Julia J <juliayoone.jung@cgi.com>
2025-02-15 10:19:50 -0500

    test with API driven workflow

--- main.tf
+++ main.tf
@@ -2,17 +2,44 @@ terraform {
   required_version = ">=1.3.0"
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">3.43.0"
+      "source" = "hashicorp/azurerm"
+      version  = "3.43.0"
+    }
+  }
+
+  //value from https://app.terraform.io/app/Azure-CyberArk/workspaces/TerraformAzure
+  cloud {
+    organization = "Azure-CyberArk"
+    workspaces {
+      name = "TerraformAzureTest"
     }
   }
 }
 
-resource "azurerm_storage_account" "securestorage" {
-  resource_group_name           = var.resource_group_name
-  location                      = var.location
-  name                          = var.storage_account_name
-  account_tier                  = var.account_tier
-  account_replication_type      = var.account_replication_type
-  public_network_access_enabled = false
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+
+  //required since new version 
+  subscription_id = "9734ed68-621d-47ed-babd-269110dbacb1"
+  //resource_provider_registrations = "none"
+}
+
+resource "random_string" "uniquestring" {
+  length  = 20
+  special = false
+  upper   = false
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "1-e257913a-playground-sandbox"
+  location = "eastus"
+}
+
+resource "azurerm_storage_account" "storageaccount" {
+  name                     = "stg${random_string.uniquestring.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
