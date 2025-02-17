8a3570cc47e15b8adc13769fe3b246895b0e1aa1
Julia J <juliayoone.jung@cgi.com>
2025-02-17 14:27:36 -0500

    test variables

--- variables.tf
+++ variables.tf
@@ -1,27 +1,30 @@
 variable "resource_group_name" {
-  description = "Name of provided resource group"
-  type = string
+  description = "Name of provided resource group provided by the lab"
+  type        = string
+  nullable    = false
 }
 
 variable "location" {
   description = "the location of resourec group"
-  type = string
+  type        = string
   default     = "eastus"
 }
 
 variable "subscription_id" {
   description = "id of subscription which you can find from resource group"
-  type = string
+  type        = string
 }
 
-# variable "cloud_org" {
-#   description = "The organization of terraform cloud"
-#   type        = string
-#   default     = "Azure-CyberArk"
-# }
+/*
+variable "cloud_org" {
+  description = "The organization of terraform cloud"
+  type        = string
+  default     = "Azure-CyberArk"
+}
 
-# variable "cloud_name" {
-#   description = "The name of terraform cloud which you can find from app.terraform.io"
-#   type        = string
-#   default     = "TerraformAzureTest"
-# }
+variable "cloud_name" {
+  description = "The name of terraform cloud which you can find from app.terraform.io"
+  type        = string
+  default     = "TerraformAzureTest"
+}
+*/
