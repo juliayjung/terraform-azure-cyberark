e089b78174f4b1006a9778dce684951eb255fe15
juliayjung <126819755+juliayjung@users.noreply.github.com>
2025-03-13 12:17:51 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "storageaccount" {
-  name                     = "stg${random_string.uniquestring.result}"
+  name                     = "strg${random_string.uniquestring.result}"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
